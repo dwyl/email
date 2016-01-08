e7e62c84675320d7ed5d9073f9bda65fcca6740a
nelsonic <contact.nelsonic+github@gmail.com>
2016-01-08 07:57:46 +0000

    borrows google OAuth2 code from https://github.com/dwyl/team-calendar/tree/9f1f437e1753ea1ccd2edaf6e75c839b2583786d fixes https://github.com/nelsonic/sendemail-gmail/issues/3

---
+++ server.js
@@ -0,0 +1,91 @@
+require('env2')('.env');
+var assert = require('assert');
+var Hapi   = require('hapi'); // require the hapi module
+var server = new Hapi.Server();
+
+server.connection({
+	host: 'localhost',
+	port: Number(process.env.PORT) // defined by environment variable or .env file
+});
+
+var scopes = [
+  'https://www.googleapis.com/auth/plus.profile.emails.read',
+  'https://www.googleapis.com/auth/calendar.readonly'
+]
+
+var opts = {
+  REDIRECT_URL: '/googleauth',  // must match google app redirect URI
+  handler: require('./lib/google_oauth_handler.js'), // your handler
+  scope: scopes // profile
+};
+
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+var gcal = google.calendar('v3'); // http://git.io/vBGLn
+var oauth2Client = new OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, opts.REDIRECT_URL);
+
+
+var hapi_auth_google = require('hapi-auth-google');
+
+var plugins = [
+	{ register: hapi_auth_google, options:opts },
+	require('hapi-auth-jwt2')
+];
+server.register(plugins, function (err) {
+  // handle the error if the plugin failed to load:
+  assert(!err, "FAILED TO LOAD PLUGIN!!! :-("); // fatal error
+	// see: http://hapijs.com/api#serverauthschemename-scheme
+  server.auth.strategy('jwt', 'jwt', true,
+  { key: process.env.JWT_SECRET,
+    validateFunc: require('./lib/hapi_auth_jwt2_validate.js'),
+    verifyOptions: { ignoreExpiration: true }
+  });
+
+  server.route([{
+    method: 'GET',
+    path: '/',
+    config: { auth : false },
+    handler: function(request, reply) {
+      var url    = server.generate_google_oauth2_url();
+  		var imgsrc = 'https://developers.google.com/accounts/images/sign-in-with-google.png';
+  		var btn    = '<a href="' + url +'"><img src="' +imgsrc +'" alt="Login With Google"></a>'
+      reply(btn);
+    }
+  },
+  {
+    method: 'GET',
+    path: '/calendar',
+    config: { auth : 'jwt' },
+    handler: function(request, reply) {
+      console.log('- - - - -> '+ request.auth.credentials.emails[0].value);
+      oauth2Client.setCredentials(request.auth.credentials.tokens);
+      var email = request.auth.credentials.emails[0].value;
+      gcal.events.list({ calendarId: email, auth: oauth2Client }, function(err, response) {
+        console.log(' - - - - - - - - - - - - - - - - - - calendar api err:');
+        console.log(err)
+        console.log(' - - - - - - - - - - - - - - - - - - calendar api response:');
+        console.log(response);
+        // handle err and response
+        reply('<pre><code>'+JSON.stringify(response, null, 2)+'</code></pre>');
+      });
+      // gcal.calendarList.list({ auth: oauth2Client }, function(err, response) {
+      //   console.log(' - - - - - - - - - - - - - - - - - - calendar api err:');
+      //   console.log(err)
+      //   console.log(' - - - - - - - - - - - - - - - - - - calendar api response:');
+      //   console.log(response);
+      //   // handle err and response
+      //   reply('<pre><code>'+JSON.stringify(response, null, 2)+'</code></pre>');
+      // });
+    }
+  }
+
+  ]);
+
+});
+
+server.start(function(err){ // boots your server
+  assert(!err, "FAILED TO Start Server", err);
+	console.log('Now Visit: http://localhost:'+server.info.port);
+});
+
+module.exports = server;
---
+++ test/server.test.js
@@ -0,0 +1,113 @@
+var test = require('tape');
+var redisClient = require('redis-connection')(); // instantiate redis-connection
+var dir  = __dirname.split('/')[__dirname.split('/').length-1];
+var file = dir + __filename.replace(__dirname, '') + " > ";
+var JWT  = require('jsonwebtoken');
+var server = require('../server.js');
+
+test(file + 'test the response on / is a link to Google Auth', function(t){
+  var options = {
+    method: "GET",
+    url: "/"
+  };
+
+  server.inject(options, function(response) {
+    console.log(response.result);
+    t.equal(response.result.indexOf('sign-in-with-google.png') > -1, true, 'Displays link');
+    setTimeout(function(){
+      // redisClient.end();   // ensure redis con closed! - \\
+      // t.equal(redisClient.connected, false, "✓ Connection to Redis Closed");
+      server.stop(t.end)
+    }, 100);
+  });
+});
+
+test(file+'Ensure the server does not 500 when /googleauth?code=badcode', function(t) {
+  var options = {
+    method: "GET",
+    url: "/googleauth?code=badcode"
+  };
+  server.inject(options, function(response) {
+    t.equal(response.statusCode, 200, "Server is working.");
+    t.ok(response.payload.indexOf('something went wrong') > -1,
+          'Got: '+response.payload + ' (As Expected)');
+    server.stop(function(){ });
+    t.end();
+  });
+});
+
+var COOKIE; // we get this in the response in the next test:
+
+test(file+'MOCK Google OAuth2 Flow /googleauth?code=mockcode', function(t) {
+  // google oauth2 token request url:
+  var fs = require('fs');
+  var token_fixture = fs.readFileSync('./test/fixtures/sample-auth-token.json');
+  var nock = require('nock');
+  var scope = nock('https://accounts.google.com')
+            .persist() // https://github.com/pgte/nock#persist
+            .post('/o/oauth2/token')
+            .reply(200, token_fixture);
+
+  // see: http://git.io/v4nTR for google plus api url
+  // https://www.googleapis.com/plus/v1/people/{userId}
+  var sample_profile = fs.readFileSync('./test/fixtures/sample-profile.json');
+  var nock = require('nock');
+  var scope = nock('https://www.googleapis.com')
+            .get('/plus/v1/people/me')
+            .reply(200, sample_profile);
+
+
+  var options = {
+    method: "GET",
+    url: "/googleauth?code=mockcode"
+  };
+  server.inject(options, function(response) {
+    t.equal(response.statusCode, 200, "Profile retrieved (Mock)");
+    var expected = 'Logged in Using Google!';
+    t.ok(response.payload.indexOf(expected) > -1, "Got: " + expected + " (as expected)");
+    console.log(' - - - - - - - - - - - - - - - - - - cookie:');
+    console.log(response.headers);
+    COOKIE = response.headers['set-cookie'][0]; //.split('=')[1];
+    // console.log(COOKIE);
+    // console.log(' - - - - - - - - - - - - - - - - - - decoded:');
+    // console.log(JWT.decode(COOKIE));
+    server.stop(t.end);
+  });
+});
+
+test(file+'Visit /calendar using JWT Cookie', function(t) {
+  console.log(' - - - - - - - - - - - - - - - - - - cookie:');
+  console.log(COOKIE);
+  var options = {
+    method: "GET",
+    url: "/calendar",
+    headers: { cookie: COOKIE }
+  };
+  server.inject(options, function(response) {
+    t.equal(response.statusCode, 200, "Successfully showing /calendar page");
+    // setTimeout(function(){ server.stop(t.end); }, 100);
+    server.stop(function(){
+      t.end()
+    });
+  });
+});
+
+test(file+'Visit /calendar with INVALID JWT Cookie', function(t) {
+  var token = JWT.sign({ id: 321, "name": "Charlie" }, process.env.JWT_SECRET);
+  var options = {
+    method: "GET",
+    url: "/calendar",
+    headers: { cookie: "token=" + token }
+  };
+  server.inject(options, function(response) {
+    // console.log(' - - - - - - - - - - - - - - - - - - result:');
+    // console.log(response.result);
+    t.equal(response.statusCode, 401, "Auth Blocked by bad Cookie JWT");
+    // setTimeout(function(){ server.stop(t.end); }, 100);
+    server.stop(function(){
+      redisClient.end();   // ensure redis con closed! - \\
+      t.equal(redisClient.connected, false, "✓ Connection to Redis Closed");
+      t.end()
+    });
+  });
+});
