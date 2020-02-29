06ad5a5d429388f05279e123327e2d7994444bef
nelsonic <nelson+github@dwyl.io>
2020-02-29 23:37:09 +0000

    create elixir_buildpack.config to use latest version of Elixir on Heroku

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule App.MixProject do
     [
       app: :app,
       version: "1.0.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
