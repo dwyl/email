9cbc2d7ab939c8607433e84ce67b6c3dcca87a08
nelsonic <nelson+github@dwyl.io>
2020-03-01 01:14:09 +0000

    use elixir 1.10 for #21

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule App.MixProject do
     [
       app: :app,
       version: "1.0.0",
-      elixir: "~> 1.10.2",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
