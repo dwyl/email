9f3341d1526b1c76be05b0e370c5fdb9ce76b75a
nelsonic <nelson+github@dwyl.io>
2020-03-12 15:26:40 +0000

    add import Phoenix.LiveView{Controller,Helpers,Router} to lib/app_web.ex

--- lib/app_web.ex
+++ lib/app_web.ex
@@ -24,6 +24,7 @@ defmodule AppWeb do
       import Plug.Conn
       import AppWeb.Gettext
       alias AppWeb.Router.Helpers, as: Routes
+      import Phoenix.LiveView.Controller
     end
   end
 
@@ -42,6 +43,7 @@ defmodule AppWeb do
       import AppWeb.ErrorHelpers
       import AppWeb.Gettext
       alias AppWeb.Router.Helpers, as: Routes
+      import Phoenix.LiveView.Helpers
     end
   end
 
@@ -50,6 +52,7 @@ defmodule AppWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
+      import Phoenix.LiveView.Router
     end
   end
 
