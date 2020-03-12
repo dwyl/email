80acd26c5dc1578b615079a51baefb384b43e598
nelsonic <nelson+github@dwyl.io>
2020-03-12 16:12:34 +0000

    add liveview to app.js (Step 9) for #23

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,9 @@ import css from "../css/app.css"
 //
 import "phoenix_html"
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+import {Socket} from "phoenix"
+import LiveSocket from "phoenix_live_view"
+
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}});
+liveSocket.connect()
