739305735b2984603f6eb60444452bb1ea0b13d3
nelsonic <nelson+github@dwyl.io>
2020-03-16 15:29:30 +0000

    render sent items as table

--- lib/app_web/live/dashboard.ex
+++ lib/app_web/live/dashboard.ex
@@ -5,17 +5,24 @@ defmodule AppWeb.Dashboard do
 
     def mount(_session, _params, socket) do
       AppWeb.Endpoint.subscribe(@topic) # subscribe to the channel
-      {:ok, assign(socket, :val, 0),
+      sent = App.Ctx.list_sent()
+      {:ok, assign(socket, %{val: 0, sent: sent}),
         layout: {AppWeb.LayoutView, "live.html"}}
     end
 
     def handle_event("inc", _value, socket) do
+      sent = App.Ctx.list_sent()
+      IO.inspect(socket, label: "socket")
       new_state = update(socket, :val, &(&1 + 1))
+      IO.inspect(socket, label: "socket")
+      # new_state = update(socket, :sent, sent)
       AppWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
       {:noreply, new_state}
     end
 
     def handle_event("dec", _, socket) do
+      # sent = App.Ctx.list_sent()
+
       new_state = update(socket, :val, &(&1 - 1))
       AppWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
       {:noreply, new_state}
