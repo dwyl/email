4d20c77a70578d51370ce81d99e8f4a5d0cf449f
nelsonic <nelson+github@dwyl.io>
2020-03-21 16:04:55 +0000

    fix indentation of dashboard.ex

--- lib/app_web/live/dashboard.ex
+++ lib/app_web/live/dashboard.ex
@@ -1,26 +1,25 @@
 defmodule AppWeb.Dashboard do
   use Phoenix.LiveView
+  @topic "live"
 
-    @topic "live"
+  def mount(_session, _params, socket) do
+    AppWeb.Endpoint.subscribe(@topic) # subscribe to the channel
+    sent = App.Ctx.list_sent_with_status()
+    {:ok, assign(socket, %{val: 0, sent: sent}),
+      layout: {AppWeb.LayoutView, "live.html"}}
+  end
 
-    def mount(_session, _params, socket) do
-      AppWeb.Endpoint.subscribe(@topic) # subscribe to the channel
-      sent = App.Ctx.list_sent_with_status()
-      {:ok, assign(socket, %{val: 0, sent: sent}),
-        layout: {AppWeb.LayoutView, "live.html"}}
-    end
+  def handle_event("sent", _value, socket) do
+    new_state = update(socket, :sent, App.Ctx.list_sent_with_status())
+    AppWeb.Endpoint.broadcast_from(self(), @topic, "sent", new_state.assigns)
+    {:noreply, new_state}
+  end
 
-    def handle_event("sent", _value, socket) do
-      new_state = update(socket, :sent, App.Ctx.list_sent_with_status())
-      AppWeb.Endpoint.broadcast_from(self(), @topic, "sent", new_state.assigns)
-      {:noreply, new_state}
-    end
+  def handle_info(_msg, socket) do
+    {:noreply, assign(socket, sent: App.Ctx.list_sent_with_status())}
+  end
 
-    def handle_info(_msg, socket) do
-      {:noreply, assign(socket, sent: App.Ctx.list_sent_with_status())}
-    end
-
-    def render(assigns) do
-      AppWeb.PageView.render("dashboard.html", assigns)
-    end
+  def render(assigns) do
+    AppWeb.PageView.render("dashboard.html", assigns)
+  end
 end
