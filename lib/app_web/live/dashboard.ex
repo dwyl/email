bca378e7b14b21f99cbc37ffcaf897cd603c9d49
nelsonic <nelson+github@dwyl.io>
2020-03-21 13:25:38 +0000

    tidy up dashboard.ex (remove IO.inspect) #23

--- lib/app_web/live/dashboard.ex
+++ lib/app_web/live/dashboard.ex
@@ -6,16 +6,12 @@ defmodule AppWeb.Dashboard do
     def mount(_session, _params, socket) do
       AppWeb.Endpoint.subscribe(@topic) # subscribe to the channel
       sent = App.Ctx.list_sent_with_status()
-      # IO.inspect(sent, label: "sent")
       {:ok, assign(socket, %{val: 0, sent: sent}),
         layout: {AppWeb.LayoutView, "live.html"}}
     end
 
     def handle_event("sent", _value, socket) do
-      # IO.inspect(socket, label: "socket")
       new_state = update(socket, :sent, App.Ctx.list_sent_with_status())
-      # IO.inspect(socket, label: "socket")
-      # new_state = update(socket, :sent, sent)
       AppWeb.Endpoint.broadcast_from(self(), @topic, "sent", new_state.assigns)
       {:noreply, new_state}
     end
--- test/app_web/live/dashboard_test.exs
+++ test/app_web/live/dashboard_test.exs
@@ -9,13 +9,18 @@ defmodule AppWeb.DashboardTest do
   #   {:ok, socket: socket}
   # end
 
-  # describe "AppWeb.Dashboard > " do
-  #   test "handle_event/3", %{socket: socket} do
-  #     IO.inspect(socket, label: "socket 6")
-  #     # {:noreply, new_state} = AppWeb.Dashboard.handle_event("sent", "", socket)
-  #     # IO.inspect(new_state, label: "new_state 7")
-  #     # conn = get(conn, Routes.sent_path(conn, :index))
-  #     # assert html_response(conn, 200) =~ "Listing Sent"
-  #   end
-  # end
+  describe "AppWeb.Dashboard > " do
+    # test "hello" do
+    #   IO.inspect("hello")
+    # end
+    test "handle_event/3" do
+      socket = %Phoenix.LiveView.Socket{}
+
+      IO.inspect(socket, label: "socket 20")
+      AppWeb.Dashboard.handle_event("sent", "", socket)
+      # IO.inspect(new_state, label: "new_state 21")
+      # conn = get(conn, Routes.sent_path(conn, :index))
+      # assert html_response(conn, 200) =~ "Listing Sent"
+    end
+  end
 end
