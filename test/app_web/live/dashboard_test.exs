2a5332328a8a25d1f8cdc74b7509865b93af8fd3
nelsonic <nelson+github@dwyl.io>
2020-03-25 16:49:12 +0000

    delete handl_event/3 function as unused/untested fixes #26

--- test/app_web/live/dashboard_test.exs
+++ test/app_web/live/dashboard_test.exs
@@ -3,13 +3,12 @@ defmodule AppWeb.DashboardTest do
   import Phoenix.LiveViewTest
 
   describe "AppWeb.Dashboard > " do
-test "handle_event/3", %{conn: conn} do
-  {:ok, view, html} = live(conn, "/")
-  # IO.inspect(view, label: "view")
-  assert view.module == AppWeb.Dashboard
-  assert html =~ "Email"
-  send(view.pid, %{refresh: 1})
-  Process.exit(view.pid, :kill)
-end
+    test "handle_event/3", %{conn: conn} do
+      {:ok, view, html} = live(conn, "/")
+      assert view.module == AppWeb.Dashboard
+      assert html =~ "Email"
+      send(view.pid, %{refresh: 1})
+      Process.exit(view.pid, :kill)
+    end
   end
 end
