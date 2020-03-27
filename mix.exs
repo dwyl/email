c654cf6d73a31c742cbad4f86413c95943a401d4
nelsonic <nelson+github@dwyl.io>
2020-03-27 11:14:47 +0000

    remove quotes from mix.exs returning response_time instead of a random quote. #30

--- mix.exs
+++ mix.exs
@@ -59,9 +59,6 @@ defmodule App.MixProject do
       {:poison, "~> 3.0"},
       {:hackney, "~> 1.9"},
 
-      # https://github.com/dwyl/quotes
-      {:quotes, "~> 1.0.5"},
-
       # LiveView for Realtime Dashboard! github.com/dwyl/email/issues/23
       {:phoenix_live_view, "~> 0.10.0"},
       {:floki, ">= 0.0.0", only: :test},
